9ef36ac9b3f57a04e85bc124b6d0eb3932555736
LucasVM22 <106699748+LucasVM22@users.noreply.github.com>
2022-06-01 16:51:06 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 #include <math.h>
-
 int main(){  
-int num1, num2, operacao;
-    printf("\t\tCalculadora\n");
-    printf("Escolha dois numeros inteiros a serem processados:\n ");
-    scanf("%i %i", &num1, &num2);
-    printf("Qual operacao voce deseja efetuar?\n Digite o numero correspondente:\n");
+    float num1, num2;
+    int operacao;
+    
+        printf("\t\tCalculadora\n");
+        printf("Escolha dois numeros reais a serem processados:\n ");
+        scanf("%f %f", &num1, &num2);
+        printf("Qual operacao voce deseja efetuar?\n Digite o numero correspondente:\n");
       printf("1. Soma\n2. Subtracao\n3. Multiplicacao\n4. Divisao\n5. Potenciacao\n");
-  scanf("%d", &operacao);
+        scanf("%d", &operacao);
 
-  switch(operacao) {
-    case 1: printf("%d", num1+num2); break;
-    case 2: printf("%d", num1-num2); break;
-    case 3: printf("%d", num1*num2); break;
-    case 4: printf("%d", num1/num2); break;
-    case 5: printf("%f", pow(num1,num2)); break;
-    default: printf("\nEssa operação não existe.\n"); break;
-  }
+    switch(operacao) {
+        case 1: printf("%f", num1+num2); break;
+        case 2: printf("%f", num1-num2); break;
+        case 3: printf("%f", num1*num2); break;
+        case 4: printf("%.2f", num1/num2); break;
+        case 5: printf("%f", pow(num1,num2)); break;
+        default: printf("\nEssa operação não existe.\n"); break;
+    }
 
     return 0;
 }
+
